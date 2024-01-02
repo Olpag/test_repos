65ed175411d7f32a1cb27a2fcdab42777899619f
Oleg Pag <olpag@ukr.net>
2024-01-03 00:14:38 +0200

    4 local from win 10

--- some_file.py
+++ some_file.py
@@ -2,3 +2,5 @@ print("This is a file from Github")
 print("There are new local changes!")
 print("There are thirds local changes!")
 print("There are remote changes on Github!")
+
+print("There are 4th local changes!")
